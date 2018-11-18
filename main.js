ba1e59d898c65d661eb427ae29c46a27c04a5ad4
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2018-11-18 21:46:11 +0530

    Life starts (✿ ♥‿♥)

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
