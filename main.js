fa1b44bfdaf1bf7cdb7521bf2c220d76fec0a1ee
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2018-11-19 16:24:09 +0530

    Should have auto update functionality

--- main.js
+++ main.js
@@ -1,28 +1,82 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, protocol, ipcMain } = require('electron');
+const log = require('electron-log');
+const { autoUpdater } = require("electron-updater");
 
-let mainWindow
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
-const createWindow = () => {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  mainWindow.loadFile('index.html')
-
-  mainWindow.on('closed', function () {
-    mainWindow = null
+let template = []
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
   })
 }
 
-app.on('ready', createWindow)
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+let win;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+function createDefaultWindow() {
+  win = new BrowserWindow();
+  win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null;
+  });
+  win.loadFile('index.html');
+  return win;
+}
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
 })
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
 })
 
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+})
+
+app.on('ready', function () {
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+  createDefaultWindow();
+  autoUpdater.checkForUpdatesAndNotify();
+
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.send('version', app.getVersion())
+  })
+
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
--- renderer.js
+++ renderer.js
@@ -1,1 +1,13 @@
+const { ipcRenderer } = require('electron');
 
+ipcRenderer.on('message', function(event, text) {
+  var container = document.getElementById('messages');
+  var message = document.createElement('div');
+  message.innerHTML = text;
+  container.appendChild(message);
+})
+
+ipcRenderer.on('version', function(event, text) {
+    console.log(text);
+    document.getElementById('version').innerText = text;
+})
